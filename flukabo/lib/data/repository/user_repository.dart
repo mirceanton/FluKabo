93f0edb85dee72b3f0c7ac0f889c8e0f0ab2c0e7
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-08-31 11:20:23 +0300

    Add 'createUser' method

--- flukabo/lib/data/repository/user_repository.dart
+++ flukabo/lib/data/repository/user_repository.dart
@@ -9,4 +9,30 @@ class UserRepository {
   factory UserRepository() => _instance;
   UserRepository._constructor(); // empty constructor
 
+  Future<void> createUser({
+    @required String username,
+    @required String password,
+    String name = '',
+    String email = '',
+    String role = 'app-user',
+  }) async {
+    final String response = await KanboardAPI().getString(
+      command: userCommands[UserProcedures.create],
+      params: {
+        'username': username,
+        'password': password,
+        'name': name,
+        'email': email,
+        'role': role,
+      },
+    );
+    final int statusCode = response == 'false' ? 0 : int.parse(response);
+    if (statusCode == 0) {
+      print('Failed to add user');
+      return false;
+    } else {
+      print('User added succesfully. UID: $statusCode');
+      return true;
+    }
+  }
 }
