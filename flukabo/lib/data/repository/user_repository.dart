75f752c97a1794c10121df57cb2d1580e87ac36f
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-01 14:49:57 +0300

    Add missing ')'

--- flukabo/lib/data/repository/user_repository.dart
+++ flukabo/lib/data/repository/user_repository.dart
@@ -72,7 +72,7 @@ class UserRepository {
       command: userCommands[UserProcedures.getById],
       params: {'user_id': id.toString()},
     );
-    final String result = jsonDecode(json)['result'].toString(;
+    final String result = jsonDecode(json)['result'].toString();
     if (result != 'null') {
       final Map<String, String> body = Map.from(result as Map<String, dynamic>);
       print('Successfully fetched user $id.');
