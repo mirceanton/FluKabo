d191efeda7d0e1b1e94b8df2b05d5115aa004373
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-02 11:48:11 +0300

    Implement 'createProject' method

--- flukabo/lib/data/repository/project_repository.dart
+++ flukabo/lib/data/repository/project_repository.dart
@@ -25,4 +25,41 @@ class ProjectRepository {
   factory ProjectRepository() => _instance;
   ProjectRepository._constructor(); // empty constructor
 
+  ///
+  /// [createProject] returns true if the Project was succesfully created or
+  /// false otherwise.
+  ///
+  /// [name] is required as all Projects must have a name
+  /// [description], [ownerId] and [identifier] are optional parameters.
+  /// If no values are provided, [description] will be empty, the [ownerId] will
+  /// be '0' and the [identifier] will also be empty
+  ///
+  /// the [id] of the newly created Project is the [statusCode] (as long as it
+  /// is not 0. then if means there was an error)
+  ///
+  Future<bool> createProject({
+    @required String name,
+    String description = '',
+    int ownerId = 0,
+    String identifier = '',
+  }) async {
+    final String json = await KanboardAPI().getJson(
+      command: projectCommands[ProjectProcedures.create],
+      params: {
+        'name': name,
+        'description': description,
+        'owner_id': ownerId.toString(),
+        'identifier': identifier,
+      },
+    );
+    final String response = jsonDecode(json)['result'].toString();
+    final int statusCode = response == 'false' ? 0 : int.parse(response);
+    if (response == 'false') {
+      print('Failed to create Project');
+      return false;
+    } else {
+      print('Project created succesfully. ProjID: $statusCode');
+      return true;
+    }
+  }
 }
