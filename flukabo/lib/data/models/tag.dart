3e159daad097f3cd327d511f883ed7b97f8277ca
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-04 11:01:43 +0300

    Add 'colorId' field and 'getColor' method

--- flukabo/lib/data/models/tag.dart
+++ flukabo/lib/data/models/tag.dart
@@ -1,17 +1,22 @@
+import 'dart:ui';
+
 import 'package:flukabo/data/helpers/json_parser.dart';
 import 'package:flukabo/data/models/project.dart';
 import 'package:flukabo/data/repository/project_repository.dart';
+import 'package:flukabo/res/kanboard/kanboard_colors.dart';
 
 class TagModel {
   int _id;
   int _projectId;
   String _name;
+  String _colorId;
 
   TagModel.empty();
   TagModel.fromJson(Map<String, dynamic> json) {
     _id = parseToInt(json['id'].toString());
     _projectId = parseToInt(json['project_id'].toString());
     _name = parseToString(json['name'].toString());
+    _colorId = parseToString(json['color_id'].toString());
   }
 
   // Getters
@@ -20,4 +25,6 @@ class TagModel {
   Future<ProjectModel> get project =>
       ProjectRepository().getProjectById(_projectId);
   String get name => _name;
+  String get colorId => _colorId;
+  Color get color => defaultColors[_colorId];
 }
