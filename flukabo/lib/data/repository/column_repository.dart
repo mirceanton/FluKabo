949e9c6de671b25457410c9ebee6d79ebb9d3af3
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-04 13:52:23 +0300

    Implement 'createColumn' method

--- flukabo/lib/data/repository/column_repository.dart
+++ flukabo/lib/data/repository/column_repository.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:flukabo/data/singletons/kanboard_api_client.dart';
+import 'package:flukabo/res/kanboard/api_procedures/column_procedures.dart';
+import 'package:flutter/material.dart';
+
 ///
 /// This is a singleton meant to encapsulate all the methods associated with
 /// the group management feature provided to admin users.
@@ -16,4 +22,39 @@ class ColumnRepository {
   factory ColumnRepository() => _instance;
   ColumnRepository._constructor(); // empty constructor
 
+  ///
+  /// [createColumn] returns true if the column was succesfully created or false
+  /// otherwise.
+  ///
+  /// [projectId] is required in order to link this column to a project board
+  /// [title] is required as all columns have to have a title up top
+  /// [taskLimit] is optional, and the default value of 0 implies that no limit
+  /// is actually set
+  /// [description] is optional and the default value is empty
+  ///
+  Future<bool> createColumn({
+    @required int projectId,
+    @required String title,
+    int taskLimit = 0,
+    String description = "",
+  }) async {
+    final String json = await KanboardAPI().getJson(
+      command: columnCommands[ColumnProcedures.create],
+      params: {
+        'project_id': projectId.toString(),
+        'title': title,
+        'task_limit': taskLimit.toString(),
+        'description': description,
+      },
+    );
+    final String response = jsonDecode(json)['result'].toString();
+    if (response == 'false' || response == 'null' || response.isEmpty) {
+      print('Failed to create group');
+      return false;
+    } else {
+      final int statusCode = response == 'false' ? 0 : int.parse(response);
+      print('Group created succesfully. GID: $statusCode');
+      return true;
+    }
+  }
 }
