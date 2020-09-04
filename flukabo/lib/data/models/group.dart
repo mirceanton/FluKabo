f23758e0c52fa880d265e2791db17e51b2fcfada
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-04 14:03:34 +0300

    Change Json constructor to accept Map<String, dynamic>

--- flukabo/lib/data/models/group.dart
+++ flukabo/lib/data/models/group.dart
@@ -7,10 +7,12 @@ class GroupModel {
       : _id = id,
         _eId = eId,
         _name = name;
-  GroupModel.fromJson(Map<String, String> json)
-      : _id = int.parse(json['id']),
-        _eId = json['external_id'].isEmpty ? 0 : int.parse(json['external_id']),
-        _name = json['name'];
+  GroupModel.fromJson(Map<String, dynamic> json)
+      : _id = int.parse(json['id'].toString()),
+        _eId = json['external_id'].toString().isEmpty
+            ? 0
+            : int.parse(json['external_id'].toString()),
+        _name = json['name'].toString();
   GroupModel.empty();
 
   int get id => _id;
