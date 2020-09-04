47b6e38ec9e4a2955aede49d8639e84001f97343
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-04 10:17:22 +0300

    Add tag api commands

---
+++ flukabo/lib/res/kanboard/api_procedures/tag_procedures.dart
@@ -0,0 +1,26 @@
+///
+/// Group API Procedures
+/// doc: https://docs.kanboard.org/en/latest/api/tags_procedures.html
+///
+/// [TagProcedures] enum of all the used API methods
+/// [tagCommands] map from [TagProcedures] -> actual api command
+///
+enum TagProcedures {
+  create,
+  getByProject,
+  getByTask,
+  getAll,
+  update,
+  remove,
+  assign,
+}
+
+const Map<TagProcedures, String> tagCommands = {
+  TagProcedures.create: 'createTag',
+  TagProcedures.getByProject: 'getTagsByProject',
+  TagProcedures.getByTask: 'getTaskTags',
+  TagProcedures.getAll: 'getAllTags',
+  TagProcedures.update: 'updateTag',
+  TagProcedures.remove: 'removeTag',
+  TagProcedures.assign: 'setTaskTags',
+};
