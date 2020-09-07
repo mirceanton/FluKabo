a89ff1fef1f49d9527c841d38275a36ecf4e50d7
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-06 17:33:24 -0700

    Implement an interface for all DataModel classes

---
+++ flukabo/lib/data/models/template_model.dart
@@ -0,0 +1,13 @@
+abstract class TemplateModel {
+  ///
+  /// returns a String containing the class name
+  /// For example, the class ColumnModel will return 'column'
+  ///
+  String get type;
+
+  ///
+  /// Will call the [.fromJson] constructor and return a new instance of the
+  /// object
+  ///
+  dynamic fromJson(Map<String, dynamic> json);
+}
