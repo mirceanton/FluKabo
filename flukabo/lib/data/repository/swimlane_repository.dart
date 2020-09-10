1981422c6c2de26b486f0828a5423d2e2abbc8d4
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-10 03:41:10 -0700

    Implement all API functions

--- flukabo/lib/data/repository/swimlane_repository.dart
+++ flukabo/lib/data/repository/swimlane_repository.dart
@@ -1,3 +1,9 @@
+import 'package:flukabo/data/models/swimlane.dart';
+import 'package:flukabo/res/kanboard/api_procedures/swimlane_procedures.dart';
+import 'package:flutter/material.dart';
+
+import '../singletons/kanboard_api_client.dart';
+
 ///
 /// This is a singleton meant to encapsulate all the methods associated with
 /// the Swimlane management
@@ -16,4 +22,136 @@ class SwimlaneRepository {
 
   factory SwimlaneRepository() => _instance;
   SwimlaneRepository._constructor(); // empty constructor
+
+  Future<int> createSwimlane({
+    @required int projectId,
+    @required String name,
+    String description,
+  }) async {
+    final int statusCode = await KanboardAPI().getInt(
+      command: swimlaneCommands[SwimlaneProcedures.create],
+      params: {
+        'project_id': projectId,
+        'name': name,
+        'description': description ?? '',
+      },
+    );
+    return statusCode;
+  }
+
+  Future<SwimlaneModel> getSwimlaneById(int swimlaneId) async {
+    final SwimlaneModel swimlane = await KanboardAPI().getObject<SwimlaneModel>(
+      command: swimlaneCommands[SwimlaneProcedures.getById],
+      params: {'swimlane_id': swimlaneId},
+    );
+    return swimlane;
+  }
+
+  Future<SwimlaneModel> getSwimlaneByName({
+    @required int projectId,
+    @required String swimlaneName,
+  }) async {
+    final SwimlaneModel swimlane = await KanboardAPI().getObject<SwimlaneModel>(
+      command: swimlaneCommands[SwimlaneProcedures.getByName],
+      params: {
+        'project_id': projectId,
+        'name': swimlaneName,
+      },
+    );
+    return swimlane;
+  }
+
+  Future<List<SwimlaneModel>> getAllSwimlanes(int projectId) async {
+    final List<SwimlaneModel> swimlanes =
+        await KanboardAPI().getObjectList<SwimlaneModel>(
+      command: swimlaneCommands[SwimlaneProcedures.getAll],
+      params: {'project_id': projectId},
+    );
+    return swimlanes;
+  }
+
+  Future<List<SwimlaneModel>> getActiveSwimlanes(int projectId) async {
+    final List<SwimlaneModel> swimlanes =
+        await KanboardAPI().getObjectList<SwimlaneModel>(
+      command: swimlaneCommands[SwimlaneProcedures.getActive],
+      params: {'project_id': projectId},
+    );
+    return swimlanes;
+  }
+
+  Future<bool> updateSwimlane({
+    @required int projectId,
+    @required int swimlaneId,
+    @required String name,
+    String description,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: swimlaneCommands[SwimlaneProcedures.update],
+      params: {
+        'project_id': projectId,
+        'swimlane_id': swimlaneId,
+        'name': name,
+        'description': description ?? '',
+      },
+    );
+    return status;
+  }
+
+  Future<bool> changeSwimlanePosition({
+    @required int projectId,
+    @required int swimlaneId,
+    @required int position,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: swimlaneCommands[SwimlaneProcedures.changePosition],
+      params: {
+        'project_id': projectId,
+        'swimlane_id': swimlaneId,
+        'position': position < 1 ? '1' : position,
+      },
+    );
+    return status;
+  }
+
+  Future<bool> disableSwimlane({
+    @required int projectId,
+    @required int swimlaneId,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: swimlaneCommands[SwimlaneProcedures.disable],
+      params: {
+        'project_id': projectId,
+        'swimlane_id': swimlaneId,
+      },
+    );
+    return status;
+  }
+
+  Future<bool> enableSwimlane({
+    @required int projectId,
+    @required int swimlaneId,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: swimlaneCommands[SwimlaneProcedures.enable],
+      params: {
+        'project_id': projectId,
+        'swimlane_id': swimlaneId,
+      },
+    );
+    return status;
+  }
+
+  Future<bool> removeSwimlane({
+    @required int projectId,
+    @required int swimlaneId,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: swimlaneCommands[SwimlaneProcedures.remove],
+      params: {
+        'project_id': projectId,
+        'swimlane_id': swimlaneId,
+      },
+    );
+    return status;
+  }
 }
