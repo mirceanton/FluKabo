b4be54f3e6339190a8c18652dac472de68168901
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-10 05:12:52 -0700

    Fix generic functions

--- flukabo/lib/data/singletons/kanboard_api_client.dart
+++ flukabo/lib/data/singletons/kanboard_api_client.dart
@@ -4,11 +4,20 @@ import 'dart:io';
 
 import 'package:flukabo/data/helpers/json_parser.dart';
 import 'package:flukabo/data/models/abstract_model.dart';
+import 'package:flukabo/data/models/swimlane.dart';
 import 'package:flukabo/data/singletons/user_preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/io_client.dart';
+
+import '../models/column.dart';
+import '../models/event.dart';
+import '../models/group.dart';
+import '../models/project.dart';
+import '../models/tag.dart';
+import '../models/task.dart';
+import '../models/user.dart';
 
 /// Custom Error class
 class Failure implements Exception {
@@ -116,10 +125,10 @@ class KanboardAPI {
       );
 
   ///
-  /// [getJson] embeds the [command] into the request and returns the raw json
+  /// [_getJson] embeds the [command] into the request and returns the raw json
   /// response body
   ///
-  Future<String> getJson({
+  Future<String> _getJson({
     @required String command,
     @required Map<String, dynamic> params,
   }) async {
@@ -132,6 +141,29 @@ class KanboardAPI {
       params: params,
     );
     return response.body;
+  }
+
+  T objectFromJson<T>(Map<String, dynamic> json) {
+    switch (T) {
+      case ColumnModel:
+        return ColumnModel.fromJson(json) as T;
+      case EventModel:
+        return EventModel.fromJson(json) as T;
+      case GroupModel:
+        return GroupModel.fromJson(json) as T;
+      case ProjectModel:
+        return ProjectModel.fromJson(json) as T;
+      case SwimlaneModel:
+        return SwimlaneModel.fromJson(json) as T;
+      case TagModel:
+        return TagModel.fromJson(json) as T;
+      case TaskModel:
+        return TaskModel.fromJson(json) as T;
+      case UserModel:
+        return UserModel.fromJson(json) as T;
+      default:
+        return null;
+    }
   }
 
   Future<bool> getBool({
@@ -178,7 +210,7 @@ class KanboardAPI {
     @required String command,
     @required Map<String, dynamic> params,
   }) async {
-    final String json = await KanboardAPI().getJson(
+    final String json = await KanboardAPI()._getJson(
       command: command,
       params: params,
     );
@@ -205,14 +237,13 @@ class KanboardAPI {
       command: command,
       params: params,
     );
-    T object;
     final Map<String, dynamic> body =
         jsonDecode(response.body)['result'] as Map<String, dynamic>;
     if (body == null) {
       print('Request failed.');
       throw const Failure('Failed request to fetch object.');
     } else {
-      object = (T as AbstractDataModel).fromJson(body) as T;
+      final T object = objectFromJson<T>(body);
       print('Successfully fetched ${object.type}');
       return object;
     }
@@ -238,8 +269,7 @@ class KanboardAPI {
       final List<T> objects = [];
       for (int i = 0; i < result.length; i++) {
         objects.add(
-          (T as AbstractDataModel).fromJson(result[i] as Map<String, dynamic>)
-              as T,
+          objectFromJson<T>(result[i] as Map<String, dynamic>),
         );
       }
       print('Successfully fetched ${objects.length} ${objects[0].type}');
