81b6547afbe02f99af95e5c7debe327e1141df48
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-11 03:50:11 -0700

    Add all time tracking api commands

--- flukabo/lib/res/kanboard/api_procedures/subtask_procedures.dart
+++ flukabo/lib/res/kanboard/api_procedures/subtask_procedures.dart
@@ -20,3 +20,24 @@ const Map<SubtaskProcedures, String> subtaskCommands = {
   SubtaskProcedures.update: 'updateSubtask',
   SubtaskProcedures.remove: 'removeSubtask',
 };
+
+///
+/// Subtask Time Trackink API Procedures
+/// doc: https://docs.kanboard.org/en/latest/api/subtask_procedures.html
+///
+/// [TimeProcedures] enum of all the used API methods
+/// [timeCommands] map from [TimeProcedures] -> actual api command
+///
+enum TimeProcedures {
+  startTimer,
+  stopTimer,
+  hasTimer,
+  getTime,
+}
+
+const Map<TimeProcedures, String> timeCommands = {
+  TimeProcedures.startTimer: 'setSubtaskStartTime',
+  TimeProcedures.stopTimer: 'setSubtaskEndTime',
+  TimeProcedures.hasTimer: 'hasSubtaskTimer',
+  TimeProcedures.getTime: 'getSubtaskTimeSpent',
+};
