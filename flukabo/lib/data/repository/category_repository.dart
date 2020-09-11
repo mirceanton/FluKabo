bc07cb42d235abd7425af3ce592237537a0eddb4
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-11 02:44:15 -0700

    Implement all functions

--- flukabo/lib/data/repository/category_repository.dart
+++ flukabo/lib/data/repository/category_repository.dart
@@ -1,3 +1,8 @@
+import 'package:flukabo/data/models/category.dart';
+import 'package:flukabo/data/singletons/kanboard_api_client.dart';
+import 'package:flukabo/res/kanboard/api_procedures/category_procedures.dart';
+import 'package:flutter/material.dart';
+
 ///
 /// This is a singleton meant to encapsulate all the methods associated with
 /// the category management
@@ -15,4 +20,56 @@ class CategoryRepository {
   factory CategoryRepository() => _instance;
   CategoryRepository._constructor(); // empty constructor
 
+  Future<int> createCategory({
+    @required String name,
+    @required int projectId,
+  }) async {
+    final int statusCode = await KanboardAPI().getInt(
+      command: categoryCommands[CategoryProcedures.create],
+      params: {
+        'name': name,
+        'project_id': projectId,
+      },
+    );
+    return statusCode;
+  }
+
+  Future<CategoryModel> getCategoryById(int categoryId) async {
+    final CategoryModel category = await KanboardAPI().getObject<CategoryModel>(
+      command: categoryCommands[CategoryProcedures.getById],
+      params: {'category_id': categoryId},
+    );
+    return category;
+  }
+
+  Future<List<CategoryModel>> getAllCategoriesForProject(int projectId) async {
+    final List<CategoryModel> categories =
+        await KanboardAPI().getObjectList<CategoryModel>(
+      command: categoryCommands[CategoryProcedures.getAll],
+      params: {'project_id': projectId},
+    );
+    return categories;
+  }
+
+  Future<bool> updateCategory({
+    @required int id,
+    @required String name,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: categoryCommands[CategoryProcedures.update],
+      params: {
+        'id': id,
+        'name': name,
+      },
+    );
+    return status;
+  }
+
+  Future<bool> removeCategory(int categoryId) async {
+    final bool status = await KanboardAPI().getBool(
+      command: categoryCommands[CategoryProcedures.remove],
+      params: {'id': categoryId},
+    );
+    return status;
+  }
 }
