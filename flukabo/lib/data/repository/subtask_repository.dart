cf74ebe1a5b69c678b789e976481bd997aca760c
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-11 03:38:06 -0700

    Implement all functions

--- flukabo/lib/data/repository/subtask_repository.dart
+++ flukabo/lib/data/repository/subtask_repository.dart
@@ -1,3 +1,8 @@
+import 'package:flukabo/data/models/subtask.dart';
+import 'package:flukabo/data/singletons/kanboard_api_client.dart';
+import 'package:flukabo/res/kanboard/api_procedures/subtask_procedures.dart';
+import 'package:flutter/cupertino.dart';
+
 ///
 /// This is a singleton meant to encapsulate all the methods associated with
 /// the subtask management
@@ -14,4 +19,76 @@ class SubtaskRepository {
 
   factory SubtaskRepository() => _instance;
   SubtaskRepository._constructor(); // empty constructor
+
+  Future<int> createSubtask({
+    @required int taskId,
+    @required String title,
+    int userId,
+    int timeEstimated,
+    int timeSpent,
+    int status,
+  }) async {
+    final int statusCode = await KanboardAPI().getInt(
+      command: subtaskCommands[SubtaskProcedures.create],
+      params: {
+        'task_id': taskId,
+        'title': title,
+        'user_id': userId ?? '',
+        'time_estimated': timeEstimated ?? 0,
+        'time_spent': timeSpent ?? 0,
+        'status': status ?? 0,
+      },
+    );
+    return statusCode;
+  }
+
+  Future<SubtaskModel> getSubtaskById(int subtaskId) async {
+    final SubtaskModel subtask = await KanboardAPI().getObject<SubtaskModel>(
+      command: subtaskCommands[SubtaskProcedures.getById],
+      params: {'subtask_id': subtaskId},
+    );
+    return subtask;
+  }
+
+  Future<List<SubtaskModel>> getAllSubtasksForTask(int taskId) async {
+    final List<SubtaskModel> subtasks =
+        await KanboardAPI().getObjectList<SubtaskModel>(
+      command: subtaskCommands[SubtaskProcedures.getAll],
+      params: {'task_id': taskId},
+    );
+    return subtasks;
+  }
+
+  Future<bool> updateSubtask({
+    @required int subtaskId,
+    @required int taskId,
+    String title,
+    int userId,
+    int timeEstimated,
+    int timeSpent,
+    int subtaskStatus,
+  }) async {
+    final SubtaskModel subtask = await getSubtaskById(subtaskId);
+    final bool status = await KanboardAPI().getBool(
+      command: subtaskCommands[SubtaskProcedures.create],
+      params: {
+        'id': subtaskId,
+        'task_id': taskId,
+        'title': title ?? subtask.title,
+        'user_id': userId ?? subtask.userId,
+        'time_estimated': timeEstimated ?? subtask.timeEstimated,
+        'time_spent': timeSpent ?? subtask.timeSpent,
+        'status': subtaskStatus ?? subtask.status,
+      },
+    );
+    return status;
+  }
+
+  Future<bool> removeSubtask(int subtaskId) async {
+    final bool status = await KanboardAPI().getBool(
+      command: subtaskCommands[SubtaskProcedures.remove],
+      params: {'subtask_id': subtaskId},
+    );
+    return status;
+  }
 }
