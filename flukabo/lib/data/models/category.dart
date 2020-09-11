5a3cde841878ca58903a758affc78664a0429a87
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-11 02:42:55 -0700

    Create Category data model

---
+++ flukabo/lib/data/models/category.dart
@@ -0,0 +1,36 @@
+import 'package:flukabo/data/helpers/json_parser.dart';
+import 'package:flukabo/data/models/abstract_model.dart';
+import 'package:flukabo/data/models/project.dart';
+import 'package:flukabo/data/repository/project_repository.dart';
+
+class CategoryModel extends AbstractDataModel {
+  int _id;
+  String _name;
+  int _projectId;
+  ProjectModel _project;
+
+  // Constructors
+  CategoryModel.empty();
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    _id = parseToInt(json['id'].toString());
+    _name = parseToString(json['name'].toString());
+    _projectId = parseToInt(json['project_id'].toString());
+
+    _project = null;
+  }
+  Future init() async {
+    _project = await ProjectRepository().getProjectById(_projectId);
+  }
+
+  // Getters
+  int get id => _id;
+  String get name => _name;
+  int get projectId => _projectId;
+  ProjectModel get project => _project;
+  @override
+  String get type => 'category';
+
+  @override
+  CategoryModel fromJson(Map<String, dynamic> json) =>
+      CategoryModel.fromJson(json);
+}
