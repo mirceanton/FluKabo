f183549eca7ee8aea5168df93d8d495b447aadb6
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-14 08:50:18 -0700

    Implement all functions

--- flukabo/lib/data/repository/task_link_repository.dart
+++ flukabo/lib/data/repository/task_link_repository.dart
@@ -1,3 +1,8 @@
+import 'package:flukabo/data/models/task_link.dart';
+import 'package:flukabo/data/singletons/kanboard_api_client.dart';
+import 'package:flukabo/res/kanboard/api_procedures/task_link_procedures.dart';
+import 'package:flutter/material.dart';
+
 ///
 /// This is a singleton meant to encapsulate all the methods associated with
 /// task link management
@@ -15,4 +20,63 @@ class TaskLinkRepository {
 
   factory TaskLinkRepository() => _instance;
   TaskLinkRepository._constructor(); // empty constructor
+
+  Future<int> createLint({
+    @required int taskId,
+    @required int oppositeTaskId,
+    @required int linkId,
+  }) async {
+    final int statusCode = await KanboardAPI().getInt(
+      command: taskLinkCommands[TaskLinkProcedures.create],
+      params: {
+        'task_id': taskId,
+        'opposite_task_id': oppositeTaskId,
+        'link_id': linkId
+      },
+    );
+    return statusCode;
+  }
+
+  Future<TaskLinkModel> getTaskLinkById(int taskLinkId) async {
+    final TaskLinkModel link = await KanboardAPI().getObject<TaskLinkModel>(
+      command: taskLinkCommands[TaskLinkProcedures.getById],
+      params: {'task_link_id': taskLinkId},
+    );
+    return link;
+  }
+
+  Future<List<TaskLinkModel>> getAllLinksForTask(int taskId) async {
+    final List<TaskLinkModel> links =
+        await KanboardAPI().getObjectList<TaskLinkModel>(
+      command: taskLinkCommands[TaskLinkProcedures.getAll],
+      params: {'task_id': taskId},
+    );
+    return links;
+  }
+
+  Future<bool> updateTaskLink({
+    @required int taskLinkId,
+    @required int taskId,
+    @required int oppositeTaskId,
+    @required int linkId,
+  }) async {
+    final bool status = await KanboardAPI().getBool(
+      command: taskLinkCommands[TaskLinkProcedures.create],
+      params: {
+        'task_link_id': taskLinkId,
+        'task_id': taskId,
+        'opposite_task_id': oppositeTaskId,
+        'link_id': linkId
+      },
+    );
+    return status;
+  }
+
+  Future<bool> removeTaskLink(int taskLinkId) async {
+    final bool status = await KanboardAPI().getBool(
+      command: taskLinkCommands[TaskLinkProcedures.remove],
+      params: {'task_link_id': taskLinkId},
+    );
+    return status;
+  }
 }
