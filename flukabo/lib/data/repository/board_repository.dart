c9688000b1b928be70a6cc314920188b55da1e45
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-14 08:07:15 -0700

    Create Board Repository

---
+++ flukabo/lib/data/repository/board_repository.dart
@@ -0,0 +1,17 @@
+import 'package:flukabo/data/models/board.dart';
+import 'package:flukabo/data/singletons/kanboard_api_client.dart';
+import 'package:flukabo/res/kanboard/api_procedures/board_procedures.dart';
+
+///
+/// This is a singleton meant to encapsulate all the methods associated with
+/// board management
+///
+/// It includes the following functionality:
+///   - Board retrieval based on project ID
+///
+class BoardRepository {
+  static final BoardRepository _instance = BoardRepository._constructor();
+
+  factory BoardRepository() => _instance;
+  BoardRepository._constructor(); // empty constructor
+}
