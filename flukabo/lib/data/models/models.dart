05bf4395afab7c4ffc1c70a109e5219d66dedaf4
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-14 08:10:44 -0700

    Create Barrell File

---
+++ flukabo/lib/data/models/models.dart
@@ -0,0 +1,16 @@
+///
+/// Barrell file to export all Data Models
+///
+export 'abstract_model.dart';
+export 'board.dart';
+export 'category.dart';
+export 'column.dart';
+export 'comment.dart';
+export 'event.dart';
+export 'group.dart';
+export 'project.dart';
+export 'subtask.dart';
+export 'swimlane.dart';
+export 'tag.dart';
+export 'task.dart';
+export 'user.dart';
