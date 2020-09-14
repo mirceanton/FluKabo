db605c6a5bc0d127856bec5f9a415959889c51c7
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-14 08:09:31 -0700

    Add missing fields + ExtendedSwimlane data model

--- flukabo/lib/data/models/swimlane.dart
+++ flukabo/lib/data/models/swimlane.dart
@@ -1,4 +1,5 @@
 import 'package:flukabo/data/models/abstract_model.dart';
+import 'package:flukabo/data/models/column.dart';
 
 import '../helpers/json_parser.dart';
 import '../repository/project_repository.dart';
@@ -11,6 +12,8 @@ class SwimlaneModel extends AbstractDataModel {
   bool _isActive;
   int _projectId;
   ProjectModel _project;
+  String _description;
+  int _taskLimit;
 
   // Constructors
   SwimlaneModel.empty();
@@ -20,6 +23,8 @@ class SwimlaneModel extends AbstractDataModel {
     _position = parseToInt(json['position'].toString());
     _isActive = parseToBool(json['is_active'].toString());
     _projectId = parseToInt(json['project_id'].toString());
+    _description = parseToString(json['description'].toString());
+    _taskLimit = parseToInt(json['task_limit'].toString());
     _project = null;
   }
   Future init() async {
@@ -33,10 +38,41 @@ class SwimlaneModel extends AbstractDataModel {
   bool get isActive => _isActive;
   int get projectId => _projectId;
   ProjectModel get project => _project;
+  String get description => _description;
+  int get taskLimit => _taskLimit;
   @override
   String get type => 'swimlane';
 
   @override
   SwimlaneModel fromJson(Map<String, dynamic> json) =>
       SwimlaneModel.fromJson(json);
+}
+
+class ExtendedSwimlaneModel extends SwimlaneModel {
+  List<ExtendedColumnModel> _columns;
+  int _columnsCount;
+  int _tasksCount;
+  int _score;
+
+  // Constructors
+  ExtendedSwimlaneModel.empty() : super.empty();
+  ExtendedSwimlaneModel.fromJson(Map<String, dynamic> json)
+      : super.fromJson(json) {
+    _columnsCount = parseToInt(json['nb_columns'].toString());
+    _tasksCount = parseToInt(json['nb_tasks'].toString());
+    _score = parseToInt(json['score'].toString());
+    _columns = parseToList<ExtendedColumnModel>(json['columns'] as List);
+  }
+
+  // Getters
+  List<ExtendedColumnModel> get columns => _columns;
+  int get columnsCount => _columnsCount;
+  int get tasksCount => _tasksCount;
+  int get score => _score;
+  @override
+  String get type => 'extended_swimlane';
+
+  @override
+  ExtendedSwimlaneModel fromJson(Map<String, dynamic> json) =>
+      ExtendedSwimlaneModel.fromJson(json);
 }
