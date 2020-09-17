f1739187e4b3259c3d341821b40f31e91affb710
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-17 14:32:41 -0700

    Remove unused field

--- flukabo/lib/bloc/data/users/states/error_state.dart
+++ flukabo/lib/bloc/data/users/states/error_state.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 import '../users_bloc.dart';
 
 class ErrorState extends UserState {
-  final int errno; // error number
   final String errmsg; // error message
-  const ErrorState({
-    this.errno,
-    @required this.errmsg,
-  });
+  const ErrorState({@required this.errmsg});
 
   @override
-  List<Object> get props => [errno, errmsg];
+  List<Object> get props => [errmsg];
 }
