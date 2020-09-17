03bed0386fb7e94595581bb49cf61338bd3aa701
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-17 13:18:14 -0700

    Create Project BLoC

---
+++ flukabo/lib/bloc/data/projects/events/events.dart
@@ -0,0 +1,7 @@
+///
+/// Barrell file to export all event classes
+///
+export 'create_events.dart';
+export 'fetch_events.dart';
+export 'remove_events.dart';
+export 'update_events.dart';
---
+++ flukabo/lib/bloc/data/projects/states/states.dart
@@ -0,0 +1,7 @@
+///
+/// Barrell file to export all state classes
+///
+export 'error_state.dart';
+export 'initial_state.dart';
+export 'loading_state.dart';
+export 'success_states.dart';
