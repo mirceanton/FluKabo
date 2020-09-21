199e8e2d18bad66d16ec298439f5df6977925bf9
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-21 06:46:58 -0700

    Implement rough UI sketch

--- flukabo/lib/ui/pages/home/tabs/tasks_tab.dart
+++ flukabo/lib/ui/pages/home/tabs/tasks_tab.dart
@@ -1,3 +1,8 @@
+import 'package:flukabo/bloc/data/tasks/events/events.dart';
+import 'package:flukabo/bloc/data/tasks/states/states.dart';
+import 'package:flukabo/bloc/data/tasks/tasks_bloc.dart';
+import 'package:flukabo/ui/templates/task/task_list_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'abstract_tab_class.dart';
@@ -14,4 +19,39 @@ class TasksTab extends HomeTab {
   HomeTabState createState() => _TasksTabState();
 }
 
-class _TasksTabState extends HomeTabState {}
+class _TasksTabState extends HomeTabState {
+  Widget _builder(BuildContext context, TasksState state) {
+    if (state is LoadingState) {
+      return buildLoadingIndicator();
+    }
+    if (state is ErrorState) {
+      return buildErrorIndicator(context);
+    }
+    if (state is SuccessState) {
+      if (state is TaskListFetchedState) {
+        return TaskListView(
+          height: double.infinity,
+          width: double.infinity,
+          tasks: state.tasks,
+        );
+      }
+    }
+    context
+        .bloc<TasksBloc>()
+        .add(const FetchAllForProjectEvent(projectId: 1, isActive: true));
+    return buildInitPage();
+  }
+
+  void _listener(BuildContext context, TasksState state) {}
+
+  @override
+  Widget buildContent() {
+    return BlocProvider(
+      create: (context) => TasksBloc(),
+      child: BlocConsumer<TasksBloc, TasksState>(
+        listener: _listener,
+        builder: _builder,
+      ),
+    );
+  }
+}
