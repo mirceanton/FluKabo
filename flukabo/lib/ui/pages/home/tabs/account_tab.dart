96b7cea18970c2cb1df4905a6591db58f4d811b9
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-21 07:16:23 -0700

    Implement user bloc

--- flukabo/lib/ui/pages/home/tabs/account_tab.dart
+++ flukabo/lib/ui/pages/home/tabs/account_tab.dart
@@ -1,3 +1,8 @@
+import 'package:flukabo/bloc/data/users/events/events.dart';
+import 'package:flukabo/bloc/data/users/states/states.dart';
+import 'package:flukabo/bloc/data/users/users_bloc.dart';
+import 'package:flukabo/data/singletons/user_preferences.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'abstract_tab_class.dart';
@@ -14,4 +19,43 @@ class AccountTab extends HomeTab {
   HomeTabState createState() => _AccountTabState();
 }
 
-class _AccountTabState extends HomeTabState {}
+class _AccountTabState extends HomeTabState {
+  Widget _builder(BuildContext context, UserState state) {
+    if (state is LoadingState) {
+      return buildLoadingIndicator();
+    } else if (state is ErrorState) {
+      return buildErrorIndicator(context);
+    } else if (state is SuccessState) {
+      if (state is UserFetchedState) {
+        return Center(child: Text(state.user.name + " - " + state.user.email));
+      }
+    }
+    // if the state is InitState, attempt a Fetch Event
+    context.bloc<UsersBloc>().add(
+          FetchByUsernameEvent(
+            username: UserPreferences().userName,
+          ),
+        );
+    return buildInitPage();
+  }
+
+  void _listener(BuildContext context, UserState state) {
+    if (state is ErrorState) {
+      showSnackbar(
+        context: context,
+        content: state.errmsg,
+      );
+    }
+  }
+
+  @override
+  Widget buildContent() {
+    return BlocProvider(
+      create: (context) => UsersBloc(),
+      child: BlocConsumer<UsersBloc, UserState>(
+        listener: _listener,
+        builder: _builder,
+      ),
+    );
+  }
+}
