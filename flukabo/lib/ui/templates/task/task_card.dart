f05d29559b81758aed9ff5b31819ce1f78e321ac
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-28 09:28:10 -0700

    Implement Task Card

---
+++ flukabo/lib/ui/templates/task/task_card.dart
@@ -0,0 +1,50 @@
+import 'package:flukabo/data/models/models.dart';
+import 'package:flukabo/ui/templates/tag/tag_list.dart';
+import 'package:flukabo/ui/templates/user/user_icon.dart';
+import 'package:flutter/material.dart';
+
+import '../template_commons.dart';
+
+class TaskCard extends StatelessWidget {
+  final TaskModel task;
+  const TaskCard({this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: InkWell(onTap: () => task.navigate(context)),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TagList(task.tags),
+                const Divider(),
+                Container(
+                  margin: const EdgeInsets.only(right: 36),
+                  child: TitleTemplate(
+                    objectId: task.id,
+                    title: task.title,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Row(children: task.modifierIcons),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 12,
+            right: 16,
+            child: UserIcon(task.owner),
+          )
+        ],
+      ),
+    );
+  }
+}
