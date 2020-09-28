3acf174e1e914804b8ed43053f6e6cb56df68d75
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-28 10:11:00 -0700

    Implement Column Template

---
+++ flukabo/lib/ui/templates/column_template.dart
@@ -0,0 +1,40 @@
+import 'package:flukabo/data/models/column.dart';
+import 'package:flutter/material.dart';
+
+class ColumnTemplate extends StatelessWidget {
+  final ExtendedColumnModel column;
+  const ColumnTemplate({this.column});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 10,
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ColumnTitle(title: column.title),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
+
+class ColumnTitle extends StatelessWidget {
+  final String title;
+  const ColumnTitle({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: Theme.of(context).accentColor,
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 1.25,
+      ),
+    );
+  }
+}
