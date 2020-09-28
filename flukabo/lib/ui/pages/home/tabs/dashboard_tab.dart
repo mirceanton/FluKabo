f53ae2107d6659845ad2e9c292eb18a39c16db09
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-28 02:24:14 -0700

    Add Listeners for BLoCs + Section Title template

--- flukabo/lib/ui/pages/home/tabs/dashboard_tab.dart
+++ flukabo/lib/ui/pages/home/tabs/dashboard_tab.dart
@@ -29,6 +29,9 @@ class DashboardTab extends HomeTab {
 }
 
 class _DashboardTabState extends HomeTabState {
+  void _projectsListener(BuildContext context, ProjectsState state) {}
+  void _taskListener(BuildContext context, TasksState state) {}
+
   Widget _projectsBuilder(BuildContext context, ProjectsState state) {
     if (state is project_states.LoadingState) {
       return buildLoading();
@@ -42,9 +45,7 @@ class _DashboardTabState extends HomeTabState {
     } else if (state is project_states.SuccessState) {
       if (state is project_states.ProjectListFetchedState) {
         if (state.projects.isEmpty) {
-          return const Center(
-            child: Text('No projects to show'),
-          );
+          return const Center(child: Text('No projects to show'));
         } else {
           return ProjectListView(
             height: cardHeight,
@@ -74,19 +75,18 @@ class _DashboardTabState extends HomeTabState {
     }
     if (state is task_states.SuccessState) {
       if (state is task_states.TaskListFetchedState) {
-        return TaskListView(
-          height: double.infinity,
-          width: double.infinity,
-          tasks: state.tasks,
-        );
+        if (state.tasks.isEmpty) {
+          return const Center(child: Text('No tasks to show'));
+        } else {
+          return TaskListView(
+            height: double.infinity,
+            width: double.infinity,
+            tasks: state.tasks,
+          );
+        }
       }
     }
-    context.bloc<TasksBloc>().add(
-          const task_events.FetchAllForProjectEvent(
-            projectId: 1,
-            isActive: true,
-          ),
-        );
+    context.bloc<TasksBloc>().add(const task_events.FetchAllOverdueEvent());
     return buildInitial();
   }
 
@@ -99,54 +99,45 @@ class _DashboardTabState extends HomeTabState {
       ],
       child: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 36,
-            padding: const EdgeInsets.all(8.0),
-            child: const Text(
-              'Starred projects',
-              style: TextStyle(
-                letterSpacing: 1.2,
-                fontSize: 16,
-              ),
-            ),
-          ),
+          const SectionTitle(title: 'Starred Projects'),
           Container(
             height: cardHeight,
             margin: const EdgeInsets.fromLTRB(0, 0, 0, 8),
             child: BlocConsumer<ProjectsBloc, ProjectsState>(
-              listener: (context, state) {},
+              listener: _projectsListener,
               builder: _projectsBuilder,
             ),
           ),
           const Divider(height: 0.5),
-          Container(
-            width: double.infinity,
-            height: 36,
-            padding: const EdgeInsets.all(8.0),
-            child: const Text(
-              'Your tasks',
-              style: TextStyle(
-                letterSpacing: 1.2,
-                fontSize: 16,
-              ),
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height -
-                cardHeight -
-                130 -
-                36 -
-                8 -
-                36 -
-                1,
-            width: double.infinity,
+          const SectionTitle(title: 'Your Tasks'),
+          Expanded(
             child: BlocConsumer<TasksBloc, TasksState>(
-              listener: (context, state) {},
+              listener: _taskListener,
               builder: _tasksBuilder,
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  const SectionTitle({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 36,
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        title,
+        style: const TextStyle(
+          letterSpacing: 1.2,
+          fontSize: 16,
+        ),
       ),
     );
   }
