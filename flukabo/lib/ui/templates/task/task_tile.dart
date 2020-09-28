363679af3d2c5126f5e44b6156c8a821ad730ed3
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-28 05:11:33 -0700

    Implement commons Widgets

--- flukabo/lib/ui/templates/task/task_tile.dart
+++ flukabo/lib/ui/templates/task/task_tile.dart
@@ -1,27 +1,34 @@
+import 'package:flukabo/ui/templates/task/task_commons.dart';
 import 'package:flutter/material.dart';
 import '../../../data/models/task.dart';
+import '../template_commons.dart';
 
 ///
 /// A basic list tile used to showcase a Task
 /// The [title] of the tile is the [task name]
 /// The [subtitle] is the [task description]
 /// The leading icon is a circle with a color based on the task priority
-/// [onTap] navigates to the TaskDetails page of [_task]
+/// [onTap] navigates to the TaskDetails page of [task]
 ///
 class TaskTile extends StatelessWidget {
-  final TaskModel _task;
-  const TaskTile(this._task);
+  final TaskModel task;
+  const TaskTile(this.task);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => _task.navigate(context),
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
-        leading: _task.buildIcon(),
-        title: _task.buildTitle(),
-        subtitle: Text(_task.description),
+    return ListTile(
+      onTap: () => task.navigate(context),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+      leading: TaskIcon(
+        priority: task.priority,
+        priorityStart: task.project == null ? 0 : task.project.priorityStart,
+        priorityEnd: task.project == null ? 0 : task.project.priorityEnd,
       ),
+      title: TitleTemplate(
+        text: task.title,
+        color: Theme.of(context).textTheme.headline6.color,
+      ),
+      subtitle: SubtitleTemplate(subtitle: task.description),
     );
   }
 }
