0964ebe3fe28a852a0c6d6c65edf32c49dac12d8
Mircea Pavel Anton <mircea.anton99@yahoo.com>
2020-09-29 03:59:33 -0700

    Move auth builder and listener to functions file

--- flukabo/lib/ui/pages/home/tabs/abstract_tab_class.dart
+++ flukabo/lib/ui/pages/home/tabs/abstract_tab_class.dart
@@ -1,10 +1,9 @@
-import 'package:flukabo/data/singletons/user_preferences.dart';
-import 'package:flukabo/ui/commons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
 import '../../../../bloc/auth/auth_bloc.dart';
+import '../../../../bloc/auth/functions.dart';
 
 ///
 /// An abstract class from which all the tabs in the HomePage are derived.
@@ -29,49 +28,6 @@ abstract class HomeTab extends StatefulWidget {
 }
 
 abstract class HomeTabState extends State<HomeTab> {
-  // Send an auth event with the fields from UserPreferences()
-  void retryAuth(BuildContext context) {
-    context.bloc<AuthBloc>().add(
-          AuthEvent(
-            url: UserPreferences().fullAddress,
-            username: UserPreferences().userName,
-            token: UserPreferences().token,
-            acceptAllCerts: UserPreferences().acceptAllCerts,
-          ),
-        );
-  }
-
-  ///
-  /// [_mainBuilder] is the builder function called by the BlocConsumer
-  /// It handles building the appropriate Layout for each state:
-  ///   - AuthLoadingState => [_buildLoadingIndicator]
-  ///   - AuthSuccessState => [buildContent]
-  ///   - AuthErrorState => [_buildError]
-  ///   - AuthInitialState => [_buildInitPage]
-  ///
-  Widget _mainBuilder(BuildContext context, AuthState state) {
-    switch (state.runtimeType) {
-      case AuthLoadingState:
-        print('Loading auth data...');
-        return buildLoading();
-      case AuthSuccessState:
-        print('Authentication successful');
-        return buildContent();
-      case AuthErrorState:
-        print('Authentication failed');
-        return buildError(
-          context,
-          icon: MdiIcons.accessPointNetworkOff,
-          message: 'Connection failed',
-          onButtonPress: () => retryAuth(context),
-        );
-      default: // this includes the [AuthInitialState]
-        print("Initial auth state");
-        retryAuth(context);
-        return buildInitial();
-    }
-  }
-
   ///
   /// [buildContent] will return the actual UI of this tab, assuming the
   /// AuthState is AuthSuccessState
@@ -83,8 +39,8 @@ abstract class HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AuthBloc, AuthState>(
-      listener: (_, __) {},
-      builder: _mainBuilder,
+      listener: listener,
+      builder: (context, state) => builder(context, state, buildContent()),
     );
   }
 }
